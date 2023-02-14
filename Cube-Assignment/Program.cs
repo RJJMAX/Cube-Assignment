01d43df211d4f6d865324473731a8a90699b190e
Rob Jaindl <robmax2023@outlook.com>
2023-02-14 10:24:32 -0500

    Add project files.

---
+++ Cube-Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿// use a while loop to pring the cube of the odd numbers from 1 to 25
+
+int x = 1;
+while (x <= 25) {
+    if (x % 2 == 0) {
+        x++;
+        continue;
+    }
+
+
+    Console.WriteLine($"{x}^3 is {x} * {x} * {x}");
+    x++;
+}
+Console.WriteLine("Done");
